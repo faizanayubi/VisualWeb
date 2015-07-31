bc8df7ab753aed79986d8f4791768f248c48289e
faizanayubi <indianayubi@gmail.com>
2015-07-31 22:50:25 +0530

    Added HTTP Library

---
+++ application/libraries/HTTP/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HTTP;
+
+/**
+ * Bootstrap file
+ * 
+ * @package HTTP
+ */
+// load class files 
+require_once './lib/HTTP/request.php';
+require_once './lib/HTTP/response.php';
