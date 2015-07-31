4df65eb1dc8ffb79b6baa1087ab673ed4e3ee9ad
faizanayubi <indianayubi@gmail.com>
2015-08-01 01:48:28 +0530

    Added Basic Nodes Method

--- application/controllers/vwa.php
+++ application/controllers/vwa.php
@@ -12,12 +12,31 @@ class VWA extends Controller {
     
     public function crawl() {
         $this->noview();
-        $scrape = new Scrape('http://swiftintern.com/');
-        var_dump($scrape);
+        $scrape = new Scrape('http://swiftintern.com/sitemap.xml');
+        $elements = $scrape->xPathObj->query('//url/loc');
+        //echo '<pre>', print_r($elements), '</pre>';
+        
+        foreach ($elements as $element) {
+            echo $element->nodeValue;
+            echo '<br>';
+        }
+    }
+    
+    public function getNodesfromXML($urls) {
+        $this->JSONview();
+    }
+    
+    public function getNodesfromURL($urls) {
+        $this->JSONview();
     }
     
     public function noview() {
         $this->willRenderLayoutView = false;
         $this->willRenderActionView = false;
+    }
+    
+    public function JSONview() {
+        $this->willRenderLayoutView = false;
+        $this->defaultExtension = "json";
     }
 }
